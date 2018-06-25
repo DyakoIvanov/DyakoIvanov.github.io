f4607842389ff0653fbacce7bcd65a868a6ed498
DyakoIvanov <35695889+DyakoIvanov@users.noreply.github.com>
2018-06-25 13:15:33 +0300

    Update 1.1.4

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $("#flip").click(function(){
-        $("#panel").slideToggle("slow");
+var w = $(window).width();
+if ( w < 1000) {
+  console.log("window width less than 1000");
+  $(function() {
+      $( ".expand" ).accordion({
+        heightStyle:"content",
+        active: false,
+        collapsible: true,
+        header:"div.accordion-header"
+      });
     });
+}
+$(window).resize(function() {
+      location.reload();
 });
